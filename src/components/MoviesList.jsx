68d872b6f37444906e85f5dadaf0719c6a5f1ea0
joao.arvins <joao.arvins@gmail.com>
2019-01-20 17:44:06 +0000

    Prevented duplicated keys: using id instead

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -4,7 +4,7 @@ import MovieItem from './MovieItem.jsx';
 const MoviesList = ({ list }) => (
     <div>
       {list.map((item) => (
-        <MovieItem key={item.title} item={item} />
+        <MovieItem key={item.id} item={item} />
       ))}
     </div>
 );
